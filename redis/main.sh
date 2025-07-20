f5d32ff229c6ef2710e526975685274ff0f3fc56
ysl2 <songliyu97@gmail.com>
2025-07-20 08:35:23 +0800

    Add redis

---
+++ redis/main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull docker.m.daocloud.io/redis:5.0.5
+
+docker run \
+    -d \
+    --name redis \
+    -p 6379:6379 \
+    docker.m.daocloud.io/redis:5.0.5 \
+    redis-server --appendonly yes --requirepass 1234
